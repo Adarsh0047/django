495b541ad5a29a6ffbbb87f918b3e5e82a499c7e
Adarsh0047 <adarshshanmugam2002@gmail.com>
2025-11-19 23:11:35 +0530

    added new code for MP92

---
+++ MP92/blogmaker_lite.py
@@ -0,0 +1,20 @@
+import os
+import django
+from django.shortcuts import render
+from django.urls import path
+from django.core.handlers.wsgi import WSGIHandler
+from django.core.management import execute_from_command_line
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+django.setup()
+
+def index(request):
+    return render(request, "index.html")
+
+urlpatterns = [
+    path("", index),
+]
+
+application = WSGIHandler()
+if __name__ == "__main__":
+    execute_from_command_line()
