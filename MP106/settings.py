47376ef0f8149b6598b3fd1598203d2e5690bd3a
Adarsh0047 <adarshshanmugam2002@gmail.com>
2025-12-01 19:11:50 +0530

    added code for MP106

--- MP106/settings.py
+++ MP106/settings.py
@@ -20,6 +20,7 @@ TEMPLATES = [
 
 INSTALLED_APPS = [
     "blogs",
+    "accounts",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -31,6 +32,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware'
 ]
@@ -51,3 +53,7 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = "staticfiles/"
+
+LOGIN_REDIRECT_URL = "blogs:index"
+
+LOGOUT_REDIRECT_URL = "blogs:index"
