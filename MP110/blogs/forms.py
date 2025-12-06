4138a7d3d0705418c406585284a4fb90340eb43c
Adarsh0047 <adarshshanmugam2002@gmail.com>
2025-12-06 15:01:40 +0530

    added mp112 code

--- MP110/blogs/forms.py
+++ MP110/blogs/forms.py
@@ -1,7 +1,12 @@
-from .models import Blog
+from .models import Blog, BlogPost
 from django import forms
 
 class BlogForm(forms.ModelForm):
     class Meta:
         model = Blog
         fields = ["title", "description"]
+
+class BlogPostForm(forms.ModelForm):
+    class Meta:
+        model = BlogPost
+        fields = ["title", "body"]
--- MP110/blogs/urls.py
+++ MP110/blogs/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path("blogs/", views.blogs, name="blogs"),
     path("blogs/<int:blog_id>", views.blog, name="blog"),
     path("posts/<int:post_id>", views.post, name="post"),
-    path("new_blogs/", views.new_blog, name="new_blog")
+    path("new_blogs/", views.new_blog, name="new_blog"),
+    path("new_posts/<int:blog_id>", views.new_blog_post, name="new_blog_post")
     ]
